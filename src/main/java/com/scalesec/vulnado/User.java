239dbde7b271ae14ecb73046ba13f7a41eb9fd96
muse-demo2 <76115619+muse-demo2@users.noreply.github.com>
2020-12-16 10:38:06 -0500

    Update User.java

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -36,8 +36,69 @@ public class User {
     }
   }
 
-  public static String encodeForSQL(String dirty) {
-    return dirty;
+  
+    public static String encodeForSQL(String x) {
+        StringBuilder sBuilder = new StringBuilder(x.length() * 11/10);
+
+        int stringLength = x.length();
+
+        for (int i = 0; i < stringLength; ++i) {
+            char c = x.charAt(i);
+
+            switch (c) {
+            case 0: /* Must be escaped for 'mysql' */
+                sBuilder.append('\\');
+                sBuilder.append('0');
+
+                break;
+
+            case '\n': /* Must be escaped for logs */
+                sBuilder.append('\\');
+                sBuilder.append('n');
+
+                break;
+
+            case '\r':
+                sBuilder.append('\\');
+                sBuilder.append('r');
+
+                break;
+
+            case '\\':
+                sBuilder.append('\\');
+                sBuilder.append('\\');
+
+                break;
+
+            case '\'':
+                sBuilder.append('\\');
+                sBuilder.append('\'');
+
+                break;
+
+            case '"': /* Better safe than sorry */
+                sBuilder.append('\\');
+                sBuilder.append('"');
+
+                break;
+
+            case '\032': /* This gives problems on Win32 */
+                sBuilder.append('\\');
+                sBuilder.append('Z');
+
+                break;
+
+            case '\u00a5':
+            case '\u20a9':
+                // escape characters interpreted as backslash by mysql
+                // fall through
+
+            default:
+                sBuilder.append(c);
+            }
+        }
+
+        return sBuilder.toString();
   }
 
   public static User fetch(String un) {
@@ -48,7 +109,7 @@ public class User {
       stmt = cxn.createStatement();
       System.out.println("Opened database successfully");
 
-      String query = "select * from users where username = '" + encodeForSQL(un) + "' limit 1";
+      String query = "select * from users where username = '" + un + "' limit 1";
       System.out.println(query);
       ResultSet rs = stmt.executeQuery(query);
       if (rs.next()) {
